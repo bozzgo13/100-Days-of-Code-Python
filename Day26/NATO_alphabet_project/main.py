cfb916d1989c084212ce0efd2826dae96be8d43f
Bostjan Tucic <bozzgo13@gmail.com>
2026-03-12 22:01:19 +0100

    Day 30 exercise: refactor Day 26 project: add error handling for user input

--- Day26/NATO_alphabet_project/main.py
+++ Day26/NATO_alphabet_project/main.py
@@ -7,8 +7,15 @@ phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
 # Create a list of the phonetic code words from a word that the user inputs.
 word = input("Enter a word: ")
-result = [phonetic_dict[letter] for letter in word.upper()]
-print(result)
+
+result = None
+try:
+    result = [phonetic_dict[letter] for letter in word.upper()]
+except KeyError:
+    print("Sorry, only letters in the alphabet please.")
+else:
+    print(result)
+
 
 # Result
 # Enter a word: Bostjan
